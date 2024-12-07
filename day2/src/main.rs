151d98999f614d88372afb083869b1b39455b02b
flow <flowsspam@gmail.com>
2024-12-07 14:02:51 +0100

    fixed day 2 and added part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -35,9 +35,65 @@ fn check_numbers_in_file(filename: &str) -> io::Result<i32> {
     Ok(good_codes)
 }
 
+fn check_code(code: Vec<u32>) -> bool {
+    
+    let is_ascending = code
+        .windows(2)
+        .all(|pair| pair[1] > pair[0] && (pair[1] - pair[0]) < 4);
+    
+    let is_descending = code
+        .windows(2)
+        .all(|pair| pair[0] > pair[1] && (pair[0] - pair[1]) < 4);
+    
+    is_ascending || is_descending
+
+}
+
+fn check_code_with_dampener(code: Vec<u32>) -> bool {
+    let mut good_code = false;
+    for i in 0..code.len() {
+        let mut code_clone = code.clone();
+        code_clone.remove(i);
+        good_code = check_code(code_clone);
+        if good_code {
+            break;
+        }
+    }
+
+    good_code
+}
+
+
+fn check_numbers_in_file_with_dampener(filename: &str) -> io::Result<i32> {
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+
+    let mut good_codes = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+        let code = parse_line_to_vector(&line);
+
+        if check_code_with_dampener(code) {
+            good_codes += 1;
+            println!("{}",line)
+        }
+    }
+    Ok(good_codes)
+}
+
+
 fn main() {
     let filename = "./input.txt";
     match check_numbers_in_file(filename) {
+        Ok(good_codes) => {
+            println!("Good codes: {:?}", good_codes);
+        }
+        Err(e) => {
+            eprintln!("Error while reading file: {}", e);
+        }
+    }
+    match check_numbers_in_file_with_dampener(filename) {
         Ok(good_codes) => {
             println!("Good codes: {:?}", good_codes);
         }
